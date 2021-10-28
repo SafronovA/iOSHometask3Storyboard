2cc3861c515cf50bee457bc101c311fd921cd1c1
SafronovA <alexey.safronov96@gmail.com>
2021-10-28 14:53:32 +0300

    Hometask 3 Storyboard

--- iOSHometask3Storyboard/ViewController.swift
+++ iOSHometask3Storyboard/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var scaleMultiplier: CGFloat = 1.0
+
+    @IBOutlet weak var scaleButton: UIButton!
+    @IBOutlet weak var contentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        scaleButton.superview!.bringSubviewToFront(scaleButton)
     }
 
 
+    @IBAction func scaleSuperview(_ sender: UIButton) {
+        if(scaleMultiplier.isEqual(to: 1.0)){
+            scaleMultiplier = 0.7;
+        } else {
+            scaleMultiplier = 1.0;
+        }
+        contentView.transform = CGAffineTransform.init(scaleX: scaleMultiplier, y: scaleMultiplier)
+    }
+    
 }
-
