597ef5d41b026fd8e434c4ce78d1fad4f4e063ae
SafronovA <alexey.safronov96@gmail.com>
2021-10-29 15:22:15 +0300

    Portrait mode is done. Need to add rotation handling

--- iOSHometask3Storyboard/ViewController.swift
+++ iOSHometask3Storyboard/ViewController.swift
@@ -8,25 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     private var scaleMultiplier: CGFloat = 1.0
-
+    
     @IBOutlet weak var scaleButton: UIButton!
     @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var botomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var trailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         scaleButton.superview!.bringSubviewToFront(scaleButton)
     }
-
-
+    
     @IBAction func scaleSuperview(_ sender: UIButton) {
+        let horizontalConstant: CGFloat
+        let verticalConstant: CGFloat
+        
         if(scaleMultiplier.isEqual(to: 1.0)){
             scaleMultiplier = 0.7;
+            let indent = (1 - scaleMultiplier)/2
+            horizontalConstant = view.frame.size.width * indent
+            verticalConstant = view.frame.size.height * indent
         } else {
             scaleMultiplier = 1.0;
+            horizontalConstant = 0
+            verticalConstant = 0
         }
-        contentView.transform = CGAffineTransform.init(scaleX: scaleMultiplier, y: scaleMultiplier)
+        topConstraint.constant = verticalConstant
+        botomConstraint.constant = verticalConstant
+        trailingConstraint.constant = horizontalConstant
+        leadingConstraint.constant = horizontalConstant
+        //        Это почему-то не работает в связке со storyboard. Работает только когда constraints in code.
+        //        contentView.transform = CGAffineTransform.init(scaleX: scaleMultiplier, y: scaleMultiplier)
     }
-    
 }
