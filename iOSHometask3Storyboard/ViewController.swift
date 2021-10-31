960a02e5a5ee9ebe4dab061e74afa85c197ad768
SafronovA <alexey.safronov96@gmail.com>
2021-10-31 17:21:50 +0300

    done

--- iOSHometask3Storyboard/ViewController.swift
+++ iOSHometask3Storyboard/ViewController.swift
@@ -18,9 +18,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
     @IBOutlet weak var topConstraint: NSLayoutConstraint!
     
+    @IBOutlet var redViewTrailingPortrait: NSLayoutConstraint!
+    @IBOutlet var redViewBottomPortrait: NSLayoutConstraint!
+    @IBOutlet var blueViewTopPortrait: NSLayoutConstraint!
+    @IBOutlet var blueViewLeadingPortrait: NSLayoutConstraint!
+    
+    @IBOutlet var redViewBottomLandscape: NSLayoutConstraint!
+    @IBOutlet var redViewTrailingLandscape: NSLayoutConstraint!
+    @IBOutlet var blueViewLeaingLandscape: NSLayoutConstraint!
+    @IBOutlet var blueViewTopLandscape: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        activateMutableConstraints()
         scaleButton.superview!.bringSubviewToFront(scaleButton)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        activateMutableConstraints();
+    }
+    
+    private func activateMutableConstraints(){
+        if UIDevice.current.orientation.isLandscape {
+            redViewTrailingPortrait.priority = .defaultLow
+            redViewBottomPortrait.priority = .defaultLow
+            blueViewTopPortrait.priority = .defaultLow
+            blueViewLeadingPortrait.priority = .defaultLow
+            
+            redViewBottomLandscape.priority = .defaultHigh
+            redViewTrailingLandscape.priority = .defaultHigh
+            blueViewLeaingLandscape.priority = .defaultHigh
+            blueViewTopLandscape.priority = .defaultHigh
+        }else{
+            redViewBottomLandscape.priority = .defaultLow
+            redViewTrailingLandscape.priority = .defaultLow
+            blueViewLeaingLandscape.priority = .defaultLow
+            blueViewTopLandscape.priority = .defaultLow
+
+            redViewTrailingPortrait.priority = .defaultHigh
+            redViewBottomPortrait.priority = .defaultHigh
+            blueViewTopPortrait.priority = .defaultHigh
+            blueViewLeadingPortrait.priority = .defaultHigh
+        }
     }
     
     @IBAction func scaleSuperview(_ sender: UIButton) {
